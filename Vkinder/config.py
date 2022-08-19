2a1e95ffcef53565ea867c8d2b5a8919c46e52ba
Oleg Shatalov <2ez4olega@mail.ru>
2022-08-19 21:28:05 +0300

    Initial Commit

--- Vkinder/config.py
+++ Vkinder/config.py
@@ -5,12 +5,12 @@ PORT = 5432
 HOST = 'localhost'
 NAME = 'nl_vkk'
 OWNER = 'test_oleg'
-PASSWORD = '12345'
+PASSWORD = ''
 
 
 # Токены группы и пользователя:
-group_token = 'vk1.a._i7QmX8C_1xqKl_-IzacnoFPVDSc1dL1K6UsZjUVQI-1r63LKABGeK38FFtUihguzcknOg2AluIioNKjLw5qx4c4OlxcA6iTBM7_EW81psFJZE_GAysljMYtj1b9DxKrJDprod_9iVslrsj8rtbiO6PzDKUAnYBjx3MhKPRzPaJ-sOpWrWLgJ9XbtmMdlZ6d'
-user_token = 'vk1.a.ET3ykTe61g5Uv5N3Vpq-Ozl1pj3XCYPUV8YHQxoeYSoc_mrSk0R2wbaEUf3BYMbHnkIPwqeatyvsYJyykS0-vMuY52Q4NBzEwqRPK_7uAGH9VegsjaYTrfliu9-0ySBGVxmoNm2R92GZ_0HymH9xDmkddL3Q2a-BhtsABP5vH34oBgBjHbKeM5VbqvZjP5H2'
+group_token = ''
+user_token = ''
 
 
 def get_token(group_token, user_token):
